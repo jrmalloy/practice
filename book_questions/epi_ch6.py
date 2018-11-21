640b7aea0e7297ea6d6f71c9638ba575a016dbc0
Jessica <malloy.j@husky.neu.edu>
2018-11-21 09:24:57 -0800

    added string problem

---
+++ book_questions/epi_ch6.py
@@ -0,0 +1,85 @@
+
+def p1_str2int(string):
+
+	if len(string) == 0:
+		print('Empty string')
+		return
+
+	digits = '0123456789'
+
+	# check if string is negative
+	if len(string) > 1 and string[0] == '-':
+		is_neg = True
+		string = string[1:]
+	else:
+		is_neg = False
+
+
+	num = 0
+
+	for ch in string:
+		idx = digits.find(ch)
+		if idx < 0:
+			print('Illegal digit')
+			return
+
+		num *= 10
+		num += idx
+
+	if is_neg:
+		return -num
+	else:
+		return num
+
+
+def	test_p1_str2int():
+	assert p1_str2int('1234') == 1234
+	assert p1_str2int('-987') == -987
+	assert p1_str2int('0') == 0
+	assert p1_str2int('') == None
+	assert p1_str2int('1234a') == None
+
+
+def p1_int2str(num):
+
+	digits = '0123456789'
+	string = []
+
+	# check if negative
+	if num < 0:
+		is_neg = True
+		num = -num
+	else:
+		is_neg = False
+
+	if not num:
+		return '0'
+
+
+	while num > 0:
+		d = num % 10
+		string.append(digits[d])
+		num = num // 10
+
+
+	if is_neg:
+		string.append('-')
+
+	string.reverse()
+
+	return ''.join(string)
+
+
+def test_p1_int2str():
+	assert p1_int2str(1234) == '1234', p1_int2str(1234)
+	assert p1_int2str(-987) == '-987', p1_int2str(-987)
+	assert p1_int2str(0) == '0', p1_int2str(0)
+
+
+
+
+
+if __name__ == '__main__': 
+	test_p1_str2int()
+	test_p1_int2str()
+
